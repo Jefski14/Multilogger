5537c640fe8167e8eb193d8ad8132b8298e52a02
Torsten Petersen <torsten96petersen@gmail.com>
2017-12-06 22:30:39 +0100

    added mehod for getting the sensornames added getConnection method for database access

--- WebApp/php/changeSensornames.php
+++ WebApp/php/changeSensornames.php
@@ -1,5 +1,24 @@
 <?php
 /**
+ Don't forget to close the connection afterwards
+ */
+function getConnection(){
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "multilogger";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname); 
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
+
+/**
  * Change the name which is associated to the sensor ID
  *
  * If the ID is not in use, this function doesn't change anything
@@ -9,33 +28,63 @@
  */
 function changeSensorName($sensorID,$newName){
     
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "multilogger";
-    
-    // Create connection
-    $conn =  new mysqli($servername, $username,$password, $dbname);
-    
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
-    // UPDATE `sensornames` SET `Name`='test' WHERE `sensorID`=900
+    $conn = getConnection();
     $sql = "UPDATE `sensornames` SET `Name`=? WHERE `sensorID`=?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("si", $Name,$sensor_ID);
     
-    
     $Name = $newName;
     $sensor_ID = $sensorID;
     $stmt->execute();
+        
+    $stmt->close();
+    $conn->close();
+}
+
+/**
+ * Change the name which is associated to the sensor name
+ *
+ * If the name is not in use, this function doesn't change anything
+ * @param Name des Sensors $oldName
+ * @param Neuer Name für diesen Sensor $newName
+ *
+ */
+function changeSensorNameByName($oldName,$newName){
     
-    echo "New records created successfully";
+    $conn = getConnection();  
+    $sql = "UPDATE `sensornames` SET `Name`=? WHERE `Name`=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $Name,$sensor_Name);
+       
+    $Name = $newName;
+    $sensor_Name = $oldName;
+    $stmt->execute();  
     
     $stmt->close();
     $conn->close();
+}
+
+
+
+function getSensorNames(){
+    $conn = getConnection();
+    $sql = "SELECT `sensorID`,`Name` FROM `sensornames`";
+    $stmt = $conn->stmt_init();
+    $stmt->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    $stack = array();
+    
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            $arr = array('ID' => $row["sensorID"],
+                'Name' => $row["Name"]);
+            array_push($stack,$arr);
+        }
+    }
+    $conn->close();
+    return json_encode($stack);
 }
 
 //changeSensorName(900, "Neuer Name des Sensors");
--- WebApp/php/insertData.php
+++ WebApp/php/insertData.php
@@ -1,4 +1,24 @@
 <?php
+
+/**
+ Don't forget to close the connection afterwards
+ */
+function getConnection(){
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "multilogger";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
 // multilogger.php
 // benötigt 6 Variablen, getrennt mittels '&'-Zeichen:
 // zum Beispiel: abcdef&&$d&&$t&&$i&&$v
@@ -38,26 +58,17 @@ $temperature = str_replace("'", " ", $temperature);
 $dateFormated = explode('-', $date);
 $datetime = $dateFormated[2].'-'.$dateFormated[1].'-'.$dateFormated[0]. " ".$time;
 
+// parse multilogger data-format to mysql date format
 if($datetime[5]== " "){
     $datetime[5]="0";
 }
-
 if($datetime[8]== " "){
     $datetime[8]="0";
 }
 
 
-// Create connection
-$servername = "localhost";
-$username   = "username";
-$password   = "password";
-$dbname     = "multilogger";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = getConnection();
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // prepare and bind
 $sql = "INSERT INTO `messwerte`(`value`, `sensorID`, `typ`, `datetime`) VALUES (?,?,?,?)";
@@ -65,13 +76,6 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("diis", $value, $sensorID, $typ,$datetime);
 $stmt->execute();
 
-$handle = fopen ("NachrichtenSpeicher.txt", w);
-fwrite ($handle, $datetime);
-fclose($handle);
-
-
-echo "New records created successfully";
-echo $date;
 
 $stmt->close();
 $conn->close();
